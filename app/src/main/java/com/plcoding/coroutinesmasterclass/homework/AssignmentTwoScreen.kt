12edcba490e2edcfe3cc5d99681b0f7609196a13
vaibhav47sharma <vaibhav47sharma@gmail.com>
2024-09-15 12:30:56 -0400

    Formatting

--- app/src/main/java/com/plcoding/coroutinesmasterclass/homework/AssignmentTwoScreen.kt
+++ app/src/main/java/com/plcoding/coroutinesmasterclass/homework/AssignmentTwoScreen.kt
@@ -36,68 +36,68 @@ import kotlinx.coroutines.withContext
 
 @Composable
 fun AssignmentTwoScreen() {
-    var photoUri by remember { mutableStateOf<Uri?>(null) }
-    val context = LocalContext.current
-    val scope = rememberCoroutineScope()
-    var isLoading by remember { mutableStateOf(false) }
-    var backgroundColor by remember {
-        mutableStateOf(Color.White)
+  var photoUri by remember { mutableStateOf<Uri?>(null) }
+  val context = LocalContext.current
+  val scope = rememberCoroutineScope()
+  var isLoading by remember { mutableStateOf(false) }
+  var backgroundColor by remember {
+    mutableStateOf(Color.White)
+  }
+
+  val imagePicker = rememberLauncherForActivityResult(
+    contract = ActivityResultContracts.PickVisualMedia()
+  ) {
+    photoUri = it
+  }
+
+  LaunchedEffect(photoUri) {
+    withContext(Dispatchers.IO) {
+      if (photoUri != null) {
+        val bitmap = context.contentResolver.openInputStream(photoUri!!).use {
+          BitmapFactory.decodeStream(it)
+        }
+        isLoading = true
+        val dominantColor = PhotoProcessor.findDominantColor(bitmap)
+        isLoading = false
+        backgroundColor = Color(dominantColor)
+      }
+    }
+  }
+
+  Column(
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally,
+    modifier = Modifier
+        .fillMaxSize()
+        .background(backgroundColor),
+  ) {
+    RotatingBoxScreen()
+    Spacer(modifier = Modifier.height(64.dp))
+    Button(onClick = {
+      scope.launch {
+        imagePicker.launch(
+          input = PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly),
+        )
+      }
+    }) {
+      Text(text = "Pick Image")
+    }
+    if (isLoading) {
+      Text(text = "Finding dominant color...", color = Color.Green)
     }
 
-    val imagePicker = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.PickVisualMedia()
-    ) {
-        photoUri = it
+    if (photoUri != null) {
+      val painter = rememberAsyncImagePainter(
+        ImageRequest
+          .Builder(context)
+          .data(data = photoUri)
+          .build()
+      )
+
+      Image(
+        painter = painter,
+        contentDescription = null
+      )
     }
-
-    LaunchedEffect(photoUri) {
-        withContext(Dispatchers.IO) {
-        if (photoUri != null) {
-            val bitmap = context.contentResolver.openInputStream(photoUri!!).use {
-                BitmapFactory.decodeStream(it)
-            }
-            isLoading = true
-            val dominantColor = PhotoProcessor.findDominantColor(bitmap)
-            isLoading = false
-            backgroundColor = Color(dominantColor)
-        }
-        }
-    }
-
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxSize()
-            .background(backgroundColor),
-    ) {
-        RotatingBoxScreen()
-        Spacer(modifier = Modifier.height(64.dp))
-        Button(onClick = {
-            scope.launch {
-                imagePicker.launch(
-                    input = PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly),
-                )
-            }
-        }) {
-            Text(text = "Pick Image",)
-        }
-        if (isLoading) {
-            Text(text = "Finding dominant color...", color = Color.Green)
-        }
-
-        if (photoUri != null) {
-            val painter = rememberAsyncImagePainter(
-                ImageRequest
-                    .Builder(context)
-                    .data(data = photoUri)
-                    .build()
-            )
-
-            Image(
-                painter = painter,
-                contentDescription = null
-            )
-        }
-    }
+  }
 }
